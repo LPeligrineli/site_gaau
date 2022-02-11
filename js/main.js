bcb019bb94a70c58298cc5e24ca641befa28020d
Leonidas Peligrineli - Framework Digital <framework47@localiza.com>
2022-02-11 14:51:55 -0300

    craido o menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('Tudo certo por aqui')
+const getMenuIcon = document.querySelector('.icon__menu')
+const getNav = document.querySelector('aside')
+
+getMenuIcon.addEventListener('click', function() {
+    console.log(getNav)
+    getNav.classList.toggle('active')
+
+})
+
