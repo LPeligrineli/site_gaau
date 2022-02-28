9442c8fea2af966b97ad56449cb442200df60b70
Leonidas Peligrineli - Framework Digital <framework47@localiza.com>
2022-02-28 10:07:04 -0300

    criada animação

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const getMenuIcon = document.querySelector('.icon__menu')
 const getNav = document.querySelector('aside')
 
 getMenuIcon.addEventListener('click', function() {
-    console.log(getNav)
     getNav.classList.toggle('active')
 
 })
