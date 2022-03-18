8c58efa50e27beb0d2fb2bad2611d85640a8a13f
Leonidas Peligrineli <l.peligrineli@gmail.com>
2022-03-18 10:15:24 -0300

    ajustes mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const getMenuIcon = document.querySelector('.icon__menu')
 const getNav = document.querySelector('aside')
+const getMovimentoGaau = document.querySelector('#movimento-gaau')
+const getMovimentoGaauImg = document.querySelector('.text_movimento > img')
 
 getMenuIcon.addEventListener('click', function() {
     getNav.classList.toggle('active')
 
 })
+
+getMovimentoGaau.addEventListener('mouseenter', function(e) {
+    getMovimentoGaauImg.setAttribute('src', './img/movimento_Gaau_focus.png')
+})
+
+getMovimentoGaau.addEventListener('mouseleave', function() {
+    getMovimentoGaauImg.setAttribute('src', './img/movimento_Gaau_initial.png')
+})
+
